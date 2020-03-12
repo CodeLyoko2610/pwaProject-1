276536e0767d268d5a065addd6abc7746bb5c7be
chuongcuu <dangchuong1999@outlook.com>
2020-03-12 18:06:50 +0200

    Add non-lifecycle event handler fetch | Override response with respondWith.

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,16 @@
+//triggered by the browser
 self.addEventListener('install', function (event) {
     console.log('[Service Worker] Installing the service worker...', event);
 })
 
+//triggered by the browser
 self.addEventListener('activate', function (event) {
     console.log('[Service Worker] Activating the service worker...', event);
     return self.clients.claim(); //Make the current service worker controls every pages under its scope immediately rather than waiting for reloading.
+})
+
+//triggered by the app itself
+self.addEventListener('fetch', function (event) {
+    console.log('[Service Worker] Fetching something...', event);
+    event.respondWith(fetch(event.request)); //override the default response example    
 })
