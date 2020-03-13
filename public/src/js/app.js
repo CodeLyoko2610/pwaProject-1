46d9838d7e4595d62cb4c7489a8f6d87bbd453cc
chuongcuu <dangchuong1999@outlook.com>
2020-03-14 01:26:47 +0200

    app.js: Handling promise reject case.

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -27,17 +27,53 @@ window.addEventListener('beforeinstallprompt', function (event) {
 //The example above. Done with Promise
 let promise = new Promise(function (resolve, reject) {
     setTimeout(function () {
-        resolve('This is executed once timer is done.');
+        //resolve case
+        //resolve('This is executed once timer is done.');
+
+        //reject case
+        reject({
+            code: 500,
+            message: "An error occurred."
+        })
     }, 3000);
 });
 
 //Chaining actions to the promise
-promise
-    .then(function (promiseResult) {
-        return promiseResult; //return an synchronous value
-    })
-    .then(function (nextResult) {
-        console.log(nextResult);
-    })
+//One way of handling promise rejection
+// promise
+//     .then(
+//         //resolve case
+//         function (promiseResult) {
+//             return promiseResult; //return an synchronous value
+//         },
+//         //reject case
+//         function (err) {
+//             console.log(err.code, err.message);
+//         }
+//     )
+//     .then(
+//         function (nextResult) {
+//             console.log(nextResult); //only run in resolve case, as no argument passed in reject case
+//         }
+//     )
 
+promise
+    .then(
+        //resolve case handler
+        function (promiseResult) {
+            return promiseResult;
+        }
+        //no handler for reject case
+    )
+    .then(
+        function (nextResult) {
+            console.log(nextResult);
+        }
+    )
+    .catch(
+        //Handling errors during the promise chain, untill before the catch, and the reject case
+        function (err) {
+            console.log(err.code, err.message);
+        }
+    )
 console.log('This is executed right after the setTimeout().')
