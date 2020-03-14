0edb4d4a5dbb8c1b286d98b844e509aa305c7765
chuongcuu <dangchuong1999@outlook.com>
2020-03-14 18:07:58 +0200

    app.js: Learning fetch() - sending GET request.

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,21 +1,24 @@
 //Register a service worker
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-        .register('/sw.js')
-        .then(function () {
-            console.log('Service worker is registered.');
-        })
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(function() {
+      console.log('Service worker is registered.');
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 }
 
 let defferedPrompt;
 //Prevent Chrome from prompting install right when criteria met
-window.addEventListener('beforeinstallprompt', function (event) {
-    console.log('beforeinstallprompt fired.');
-    defferedPrompt = event; //store the prompt event for later use
-    event.preventDefault();
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired.');
+  defferedPrompt = event; //store the prompt event for later use
+  event.preventDefault();
 
-    return false; //for not doing anything with the event
-})
+  return false; //for not doing anything with the event
+});
 
 ////Review callback functions
 // setTimeout(function () {
@@ -25,17 +28,17 @@ window.addEventListener('beforeinstallprompt', function (event) {
 // console.log('This is executed right after the setTimeout().');
 
 //The example above. Done with Promise
-let promise = new Promise(function (resolve, reject) {
-    setTimeout(function () {
-        //resolve case
-        //resolve('This is executed once timer is done.');
+let promise = new Promise(function(resolve, reject) {
+  setTimeout(function() {
+    //resolve case
+    //resolve('This is executed once timer is done.');
 
-        //reject case
-        reject({
-            code: 500,
-            message: "An error occurred."
-        })
-    }, 3000);
+    //reject case
+    reject({
+      code: 500,
+      message: 'An error occurred.'
+    });
+  }, 3000);
 });
 
 //Chaining actions to the promise
@@ -57,23 +60,37 @@ let promise = new Promise(function (resolve, reject) {
 //         }
 //     )
 
+//Common way to handle reject case
 promise
-    .then(
-        //resolve case handler
-        function (promiseResult) {
-            return promiseResult;
-        }
-        //no handler for reject case
-    )
-    .then(
-        function (nextResult) {
-            console.log(nextResult);
-        }
-    )
-    .catch(
-        //Handling errors during the promise chain, untill before the catch, and the promise's reject case
-        function (err) {
-            console.log(err.code, err.message);
-        }
-    )
-console.log('This is executed right after the setTimeout().')
+  .then(
+    //resolve case handler
+    function(promiseResult) {
+      return promiseResult;
+    }
+    //no handler for reject case
+  )
+  .then(function(nextResult) {
+    console.log(nextResult);
+  })
+  .catch(
+    //Handling errors during the promise chain, untill before the catch, and the promise's reject case
+    function(err) {
+      console.log(err.code, err.message);
+    }
+  );
+console.log('This is executed right after the setTimeout().');
+
+//Test fetch command - sending GET request
+fetch('http://httpbin.org/ip')
+  .then(function(res) {
+    console.log(res);
+
+    //Parse json to js object
+    return res.json(); //this return a promise
+  })
+  .then(function(parsedData) {
+    console.log(parsedData);
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
