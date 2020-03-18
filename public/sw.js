8333505b6011195217c289c347464837bd8fd51c
chuongcuu <dangchuong1999@outlook.com>
2020-03-18 15:04:09 +0200

    sw.js: Create/Load a sub-cache from cache sorage | Pre-cacheapp.js for later use.

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,16 @@
 //triggered by the browser
 self.addEventListener('install', function (event) {
     console.log('[Service Worker] Installing the service worker...', event);
+
+    //Pre-Caching static assets
+    //Wait untill caching is done before finishing installation SW
+    event.waitUntil(
+        caches.open('staticAsset')
+        .then(function (cache) {
+            console.log('[Service Worker] Pre-caching App Shell...');
+            cache.add('/src/js/app.js');
+        })
+    );
 })
 
 //triggered by the browser
@@ -11,5 +21,16 @@ self.addEventListener('activate', function (event) {
 
 //triggered by the app itself
 self.addEventListener('fetch', function (event) {
-    event.respondWith(fetch(event.request)); //override the default response example    
+    //Override the default response example    
+    event.respondWith(
+        //Check if there is a cache for the request
+        caches.match(event.request)
+        .then(function (response) {
+            if (response) {
+                return response;
+            } else {
+                return fetch(event.request);
+            }
+        })
+    );
 })
