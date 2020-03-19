2368bb450dc04029f04e0bc4003adb87acc99ac4
chuongcuu <dangchuong1999@outlook.com>
2020-03-19 19:34:55 +0200

    sw.js Use cache.addAll to pre-cache multiple urls (the app shell).

--- public/sw.js
+++ public/sw.js
@@ -8,7 +8,21 @@ self.addEventListener('install', function (event) {
         caches.open('staticAsset')
         .then(function (cache) {
             console.log('[Service Worker] Pre-caching App Shell...');
-            cache.add('/src/js/app.js');
+            cache.addAll([
+                '/',
+                '/index.html',
+                '/src/js/app.js',
+                '/src/js/feed.js',
+                '/src/js/polyfills/fetch.js',
+                '/src/js/polyfills/promise.js',
+                '/src/js/material.min.js',
+                '/src/css/app.css',
+                '/src/css/feed.css',
+                '/src/images/main-image.jpg',
+                'https://fonts.googleapis.com/css?family=Roboto:400,700',
+                'https://fonts.googleapis.com/icon?family=Material+Icons',
+                'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+            ]);
         })
     );
 })
