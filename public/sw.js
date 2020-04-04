c374a6d97ef0f4c7586c61dfdd32cf95491c650b
chuongcuu <dangchuong1999@outlook.com>
2020-04-04 12:16:34 +0300

    [sw.js]: Add helper function to search for passed url string in the array.

--- public/sw.js
+++ public/sw.js
@@ -15,17 +15,17 @@ const STATIC_ASSET_FILES = [
   '/src/images/main-image.jpg',
   'https://fonts.googleapis.com/css?family=Roboto:400,700',
   'https://fonts.googleapis.com/icon?family=Material+Icons',
-  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
 ];
 
 //triggered by the browser
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
   console.log('[Service Worker] Installing the service worker...', event);
 
   //Pre-Caching static assets
   //Wait untill caching is done before finishing installation SW
   event.waitUntil(
-    caches.open(STATIC_ASSET_VERSION).then(function(cache) {
+    caches.open(STATIC_ASSET_VERSION).then(function (cache) {
       console.log('[Service Worker] Pre-caching App Shell...');
       cache.addAll(STATIC_ASSET_FILES);
     })
@@ -33,16 +33,16 @@ self.addEventListener('install', function(event) {
 });
 
 //triggered by the browser
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', function (event) {
   console.log('[Service Worker] Activating the service worker...', event);
 
   //Deleting old version of sw
   event.waitUntil(
-    caches.keys().then(function(keysList) {
+    caches.keys().then(function (keysList) {
       //Execute all promises before continue
       return Promise.all(
         //Create a promises array from the string array keysList
-        keysList.map(function(key) {
+        keysList.map(function (key) {
           if (key !== STATIC_ASSET_VERSION && key !== DYNAMIC_ASSET_VERSION) {
             console.log('[Service Worker] Removing old cache...', key);
             return caches.delete(key);
@@ -55,40 +55,51 @@ self.addEventListener('activate', function(event) {
   return self.clients.claim(); //Make the current service worker controls every pages under its scope immediately rather than waiting for reloading.
 });
 
+//Helper function: check if passed string is in array
+function isInArray(string, array) {
+  for (let i = 0; i < array.Length; i++) {
+    if (string === array[i]) {
+      return true;
+    }
+    //Loop through and find nothing, return false
+    return false;
+  }
+}
+
 //CACHING STRATERGY: cache then network MIX network with cache fallback MIX cache - only
 //Intercepting all fetch requests, both from feed.js and from webpage
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
   //[Cache then network] Fetching url from feed.js, updating new version when there is network
   let url = 'https://httpbin.org/get';
   if (event.request.url.indexOf(url) > -1) {
     event.respondWith(
-      caches.open(DYNAMIC_ASSET_VERSION).then(function(cache) {
-        return fetch(event.request).then(function(response) {
+      caches.open(DYNAMIC_ASSET_VERSION).then(function (cache) {
+        return fetch(event.request).then(function (response) {
           cache.put(event.request.url, response.clone());
           return response;
         });
       })
     );
     //[Cache only] For the app shell, fetching from cache instead of network to save time - as app shell will be updated when new sw is installed
-  } else if (STATIC_ASSET_FILES.includes(event.request.url)) {
+  } else if (isInArray(event.request.url, STATIC_ASSET_FILES)) {
     event.respondWith(caches.match(event.request.url));
   }
   //[Cache with network fallback] for other urls
   else {
     event.respondWith(
-      caches.match(event.request).then(function(response) {
+      caches.match(event.request).then(function (response) {
         if (response) {
           return response; //search for request in Cache and return response
         } else {
           return fetch(event.request)
-            .then(function(res) {
-              return caches.open(DYNAMIC_ASSET_VERSION).then(function(cache) {
+            .then(function (res) {
+              return caches.open(DYNAMIC_ASSET_VERSION).then(function (cache) {
                 cache.put(event.request.url, res.clone());
                 return res; //network fallback
               });
             })
-            .catch(function(err) {
-              return caches.open(STATIC_ASSET_VERSION).then(function(cache) {
+            .catch(function (err) {
+              return caches.open(STATIC_ASSET_VERSION).then(function (cache) {
                 //Only returns offline page when requests to help page failed (other failed requests are irrelevant)
                 if (event.request.url.indexOf('/help')) {
                   return cache.match('/offline.html'); //finall cache fallback
